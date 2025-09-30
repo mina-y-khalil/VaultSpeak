fa654fba9a221d26f2936a2316d6c0294ab1d7af
mina-y-Khalil <mina.y.khalil90@gmail.com>
2025-09-30 14:48:20 -0700

    integrate axios API call into home route

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // 1. Import express and axios
 import express from 'express';
-// import axios from 'axios';
+import axios from 'axios';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
-
 
 // 2. Create an express app and set the port number.
 const app = express();
@@ -17,12 +16,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // 4. When the user goes to the home page it should render the index.ejs file.
-app.get('/', (req, res) => {
-  res.render('index.ejs');
-}); 
+// app.get('/', (req, res) => {
+//   res.render('index.ejs');
+// }); 
 
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
+app.get("/", async (req, res) => {
+    try {
+        const result = await axios.get('https://secrets-api.appbrewery.com/random')
+        const secret = result.data.secret
+        const username = result.data.username
+        res.render('index.ejs', { secret: secret, user: username });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching secret');
+    } });
 
 // 6. Listen on your predefined port and start the server.
 app.listen(PORT, () => {
